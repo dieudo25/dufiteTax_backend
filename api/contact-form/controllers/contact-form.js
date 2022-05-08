c0770a10da1927dc0646eede42e5148544f26f3d
Dieudonné TWAHIRWA <twahirwa25@gmail.com>
2022-05-08 13:36:42 +0200

    update contact content type

--- api/contact-form/controllers/contact-form.js
+++ api/contact-form/controllers/contact-form.js
@@ -5,4 +5,58 @@
  * to customize this controller
  */
 
-module.exports = {};
+ const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+ const sgMail = require('@sendgrid/mail')
+
+ module.exports = {
+   /**
+    * Create a record.
+    *
+    * @return {Object}
+    */
+ 
+    async create(ctx) {
+        let entity;
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            entity = await strapi.services.contactForm.create(data, { files });
+        } else {
+            entity = await strapi.services.contactForm.create(ctx.request.body);
+        }
+
+        entity = sanitizeEntity(entity, { model: strapi.models.contactForm });
+
+        try {
+            await strapi.plugins['email'].services.email.send({
+                to: 'twahirwa25@gmail.com',
+                from: 'dufitetax@gmail.com',
+                subject: 'Test',
+                text: `
+                    Company name : ${ entity.company_name }
+                `,
+            });
+        } catch(err) {
+            console.log(err);
+        }
+
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+        const msg = {
+            to: 'twahirwa25@gmail.com',
+            from: 'dufitetax@gmail.com',
+            subject: 'Test',
+            text: `
+                Company name : ${ entity.company_name }
+            `,
+        }
+        sgMail
+            .send(msg)
+            .then(() => {
+                console.log('Email sent')
+        }).catch((error) => {
+            console.error(error)
+        })
+
+        return entity;
+    },
+};
+  
